3ca54b098dc2dc1298cbdcb49a6283278780d566
Chris Parent <chrisnparent@gmail.com>
2024-03-07 16:21:33 -0500

    add tables and sort entries

--- src/components/TableDefinition.svelte
+++ src/components/TableDefinition.svelte
@@ -11,7 +11,7 @@
     let addEntry = () => {
         let id = Math.max(...table.entries.map(x => x.id)) + 1;
         let min = Math.max(...table.entries.map(x => x.max)) + 1;
-        table.entries = [...table.entries, { id, name: '', min, max: min, tables: [] }];
+        table.entries = [{ id, name: '', min, max: min, tables: [] }, ...table.entries];
     }
 
     let deleteEntry = (entry) => {
@@ -19,13 +19,24 @@
         table.entries =  [...table.entries.slice(0, index), ...table.entries.slice(index + 1, table.entries.length)];
     };
 
+    let sortEntries = () => {
+        table.entries.sort((a,b) => a.min - b.min);
+        table.entries = table.entries;
+    }
+
     console.log(table);
 </script>
 
-<div class="w-100 m-1">
+<div class="w-100 mb-1">
     <button on:click={() => expand = !expand} class="text-left btn btn-light border w-100">{table.name}</button>
     {#if expand}
     <div class="p-2 border">
+        <div class="d-flex">
+            <button class="btn btn-light border" on:click={addEntry}>Add Entry</button>
+            <button class="btn btn-light border" on:click={sortEntries}>Sort Entries</button>
+            <button class="btn btn-danger border" on:click={() => deleteTable(table)}>Delete Table</button>
+        </div>
+        <hr />
         <TextInput label="Display Name" bind:value={table.name} />
         <TextInput label="Category" bind:value={table.category} />
         <TextInput label="Roll" bind:value={table.roll} />
@@ -46,11 +57,7 @@
                 <TableEntry {entry} {deleteEntry} {allTables} />
                 {/each}
             </tbody>
-        </table>        
-        <div class="d-flex">
-            <button class="btn btn-light border" on:click={addEntry}>Add Entry</button>
-            <button class="btn btn-danger" on:click={() => deleteTable(table)}>Delete Table</button>
-        </div>
+        </table>
     </div>
     {/if}
 </div>
--- src/components/TableManager.svelte
+++ src/components/TableManager.svelte
@@ -1,7 +1,16 @@
 <script>
     import TableDefinition from "./TableDefinition.svelte";
+    import TextInput from "./TextInput.svelte";
 
     export let tr;
+
+    let filter = '';
+    $: filtered = tr.tables.filter(x => !filter || x.name.toLowerCase().includes(filter.toLowerCase()));
+
+    let addTable = () => {
+        let id = Math.max(...tr.tables.map(x => x.id)) + 1;
+        tr.tables = [{ id, name: 'New Table', category: '', roll: '', entries: [] }, ...tr.tables];
+    }
 
     let deleteTable = (table) => {
         if(!confirm(`Delete ${table.name}? This action is irreversible.`)) return;
@@ -9,10 +18,27 @@
         let index = tr.tables.indexOf(table);
         tr.tables = [...tr.tables.slice(0, index), ...tr.tables.slice(index + 1, tr.tables.length)];
     }
-</script>
 
+    let sortaz = () => {
+        tr.tables.sort((a,b) => a.name.localeCompare(b.name));
+        tr.tables = tr.tables;
+    }
+
+    let sortza = () => {
+        tr.tables.sort((a,b) => b.name.localeCompare(a.name));
+        tr.tables = tr.tables;
+    }
+</script>
+<div class="m-2 p-2 border">
+    <button class="btn btn-light" on:click={sortaz}>A &rarr; Z</button>
+    <button class="btn btn-light" on:click={sortza}>Z &rarr; A</button>
+    <button class="btn btn-light" on:click={addTable}>Add Table</button>
+</div>
+<div class="m-2 p-2 border">
+    <TextInput label="Filter" bind:value={filter} />
+</div>
 <div class="p-2">
-    {#each tr.tables as table (table.id)}
+    {#each filtered as table (table.id)}
         <TableDefinition {table} allTables={tr.tables} {deleteTable} />
     {/each}
 </div>
